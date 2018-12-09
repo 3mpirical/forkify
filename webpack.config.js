99c06ecdd8dec8f4d0f52011f79cda1200a9cc90
3mpirical <3mpirical10510@gmail.com>
2018-12-09 14:00:37 -0700

    added babel-polyfill to entry aray

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const Webpack = {
-    entry: "./src/js/index.js",
+    entry: ["babel-polyfill", "./src/js/index.js"],
     output: {
         path: path.resolve(__dirname, "dist/js"),
         filename: "bundle.js"
